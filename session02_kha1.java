39641625a1c2192e8bae004ec39be1b1f4f25e7a
NguyenSang94 <nguyendoansangz@gmail.com>
2025-12-05 22:44:52 +0700

    Add files via upload

---
+++ session02_kha1.java
@@ -0,0 +1,21 @@
+package session02_Kha1;
+
+import java.util.Scanner;
+
+public class session02_kha1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập 1 số nguyên dương N: ");
+        int n = Integer.parseInt(sc.nextLine());
+        if (n <= 0) {
+            System.err.println("Số nhập vào không hợp lệ");
+            return; 
+        }
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        // Xuất kết quả
+        System.out.println("Tổng các số từ 1 đến " + n + " là: " + sum);
+    }
+}
